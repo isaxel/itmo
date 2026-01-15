f49bdd4e1afaec3a491f931ffa7568034da35871
alex <124351863+isaxel@users.noreply.github.com>
2026-01-16 02:55:42 +0300

    Add files via upload

---
+++ 1st sem/opd/scr4.sh
@@ -0,0 +1,23 @@
+cd lab0
+
+cd crobat4
+
+wc -m * 2>/dev/null | sort -nr
+
+cd ..
+
+ls -R magby6 2>&1 | grep -v ':$' | sort -r 
+
+
+grep "do" crobat4/xatu crobat4/rampardos crobat4/doduo crobat4/crobat gallade6/hoppip 2>/tmp/errors_lab1
+
+
+ls -R lab0 2>/dev/null | grep '^c' | sort -k 6 -r | tail -n 3
+
+
+grep -rl "ru" 2>&1
+
+
+ls -lR | sort -k 6 -r | head -n 3
+
+cd ~
---
+++ 1st sem/opd/scr5.sh
@@ -0,0 +1,17 @@
+cd ~
+
+
+chmod -R 700 lab0
+
+
+cd lab0
+
+rm sharpedo0
+
+rm crobat4/xatu
+
+rm Copy_77
+
+rm gallade6/slowpokewalrein
+
+rm -r crobat4
